8012983e537f49c7594b21935ef7bb76cf454ee1
Xudong Huang <huangxu008@hotmail.com>
2017-01-23 14:12:49 +0800

    add benches

---
+++ benches/tcp.rs
@@ -0,0 +1,29 @@
+#![feature(test)]
+extern crate test;
+extern crate conetty;
+extern crate coroutine;
+
+use test::Bencher;
+use conetty::{Service, WireError, TcpServer, TcpClient};
+
+struct Echo;
+
+impl Service for Echo {
+    fn service(&self, request: &[u8]) -> Result<Vec<u8>, WireError> {
+        Ok(request.to_vec())
+    }
+}
+
+#[bench]
+fn echo(b: &mut Bencher) {
+    let addr = ("127.0.0.1", 2000);
+    let server = Echo.start(&addr).unwrap();
+    let client = TcpClient::connect(addr).unwrap();
+
+    b.iter(|| {
+        let _rsp = client.call_service(&vec![0; 100]).unwrap();
+    });
+
+    unsafe { server.coroutine().cancel() };
+    server.join().ok();
+}
