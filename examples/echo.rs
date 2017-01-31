dcc62dd01615b29211adb6dc2a6cc7d89af19040
Xudong Huang <huangxu008@hotmail.com>
2017-01-31 19:01:04 +0800

    update echo use trait framework

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,9 +1,14 @@
 extern crate conetty;
 extern crate coroutine;
 extern crate env_logger;
+extern crate bincode;
+#[macro_use]
+extern crate serde_derive;
 
 use std::str;
-use std::error::Error as StdError;
+use bincode::serde as encode;
+use bincode::SizeLimit::Infinite;
+// use serde::{Serialize, Deserialize};
 use conetty::{Service, Error, WireError, UdpServer, UdpClient};
 
 struct Echo;
@@ -22,25 +27,43 @@ impl EchoRpc for Echo {
 
 // ------------------------------------------------------------------------------------------------
 // below should be generated code
+#[allow(non_camel_case_types)]
+#[derive(Debug, Serialize, Deserialize)]
+enum EchoRpcEnum {
+    __invalid(()),
+    hello(String),
+}
+
 trait EchoRpcClient {
-    fn echo(&self, data: &str) -> Result<String, Error>;
+    fn echo(&self, data: String) -> Result<String, Error>;
 }
 
 impl EchoRpcClient for UdpClient {
-    fn echo(&self, data: &str) -> Result<String, Error> {
-        let ret = self.call_service(data.as_bytes())?;
-        String::from_utf8(ret).map_err(|e| Error::ClientDeserialize(e.description().into()))
+    fn echo(&self, data: String) -> Result<String, Error> {
+        let mut buf = Vec::with_capacity(1024);
+        // serialize the para
+        encode::serialize_into(&mut buf, &data, Infinite)
+            .map_err(|e| Error::ClientSerialize(e.to_string()))?;
+        // call the server
+        let ret = self.call_service(&buf)?;
+        // deserialized the response
+        encode::deserialize(&ret).map_err(|e| Error::ClientDeserialize(e.to_string()))
     }
 }
 
 impl Service for Echo {
     fn service(&self, request: &[u8]) -> Result<Vec<u8>, WireError> {
-        // do some parsing here
-        let s = str::from_utf8(request)
-            .map_err(|e| WireError::ServerDeserialize(e.description().into()))?;
+        // deserialize the request
+        let data = encode::deserialize(request)
+            .map_err(|e| WireError::ServerDeserialize(e.to_string()))?;
+        // call the service
+        let ret = self.echo(data);
         // serialize the result
-        let ret = self.echo(s.into());
-        Ok(ret.into_bytes())
+        let mut buf = Vec::with_capacity(1024);
+        encode::serialize_into(&mut buf, &ret, Infinite)
+            .map_err(|e| WireError::ServerSerialize(e.to_string()))?;
+        // send the response
+        Ok(buf)
     }
 }
 // ------------------------------------------------------------------------------------------------
@@ -54,7 +77,7 @@ fn main() {
 
     for i in 0..10 {
         let s = format!("Hello World! id={}", i);
-        let data = client.echo(&s);
+        let data = client.echo(s);
         println!("recv = {:?}", data);
     }
 
