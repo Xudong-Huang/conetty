85a870aac0dd8d17b64d97e2e325de9f2e46daa9
Xudong Huang <huangxu008@hotmail.com>
2017-01-31 09:15:29 +0800

    update echo example

--- examples/echo.rs
+++ examples/echo.rs
@@ -3,16 +3,47 @@ extern crate coroutine;
 extern crate env_logger;
 
 use std::str;
-use conetty::{Service, WireError, UdpServer, UdpClient};
+use std::error::Error as StdError;
+use conetty::{Service, Error, WireError, UdpServer, UdpClient};
 
 struct Echo;
 
-impl Service for Echo {
-    fn service(&self, request: &[u8]) -> Result<Vec<u8>, WireError> {
-        println!("req = {:?}", request);
-        Ok(request.to_vec())
+// rpc spec
+trait EchoRpc {
+    fn echo(&self, data: String) -> String;
+}
+
+// server implementation
+impl EchoRpc for Echo {
+    fn echo(&self, data: String) -> String {
+        data
     }
 }
+
+// ------------------------------------------------------------------------------------------------
+// below should be generated code
+trait EchoRpcClient {
+    fn echo(&self, data: &str) -> Result<String, Error>;
+}
+
+impl EchoRpcClient for UdpClient {
+    fn echo(&self, data: &str) -> Result<String, Error> {
+        let ret = self.call_service(data.as_bytes())?;
+        String::from_utf8(ret).map_err(|e| Error::ClientDeserialize(e.description().into()))
+    }
+}
+
+impl Service for Echo {
+    fn service(&self, request: &[u8]) -> Result<Vec<u8>, WireError> {
+        // do some parsing here
+        let s = str::from_utf8(request)
+            .map_err(|e| WireError::ServerDeserialize(e.description().into()))?;
+        // serialize the result
+        let ret = self.echo(s.into());
+        Ok(ret.into_bytes())
+    }
+}
+// ------------------------------------------------------------------------------------------------
 
 fn main() {
     env_logger::init().unwrap();
@@ -23,8 +54,8 @@ fn main() {
 
     for i in 0..10 {
         let s = format!("Hello World! id={}", i);
-        let data = client.call_service(s.as_bytes()).unwrap();
-        println!("recv = {:?}", str::from_utf8(&data).unwrap());
+        let data = client.echo(&s);
+        println!("recv = {:?}", data);
     }
 
     unsafe { server.coroutine().cancel() };
