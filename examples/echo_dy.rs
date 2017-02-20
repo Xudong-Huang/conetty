695b2dfbff4c25b7220688283f95fbe080175b70
Xudong Huang <huangxu008@hotmail.com>
2017-02-20 14:48:44 +0800

    add echo_dy example

--- examples/echo_dy.rs
+++ examples/echo_dy.rs
@@ -1,12 +1,62 @@
 #![feature(associated_consts)]
-
-extern crate conetty;
-extern crate bincode;
-extern crate env_logger;
 #[macro_use]
 extern crate tocken_id;
 #[macro_use]
 extern crate log;
+extern crate env_logger;
+extern crate conetty;
+extern crate bincode;
+
+use std::collections::HashMap;
+use conetty::coroutine::sync::RwLock;
+
+pub type DispatchFn = fn(u64, &[u8], &mut conetty::RspBuf) -> Result<(), conetty::WireError>;
+pub struct RpcServer {
+    map: RwLock<HashMap<u64, DispatchFn>>,
+}
+
+impl conetty::Server for RpcServer {
+    fn service(&self, req: &[u8], rsp: &mut conetty::RspBuf) -> Result<(), conetty::WireError> {
+        use bincode as encode;
+
+        // deserialize the request
+        let req_id: u64 = encode::deserialize(req)
+            .map_err(|e| conetty::WireError::ServerDeserialize(e.to_string()))?;
+
+        info!("req_id = {}", req_id);
+        // get the dispatch_fn
+        let handler = {
+            let r_map = self.map.read().unwrap();
+            match r_map.get(&req_id) {
+                Some(f) => *f,
+                None => return Err(conetty::WireError::Status(404)),
+            }
+        };
+
+        handler(req_id, req, rsp)
+    }
+}
+
+impl RpcServer {
+    pub fn start<L: ::std::net::ToSocketAddrs>
+        (addr: L)
+         -> ::std::io::Result<conetty::coroutine::JoinHandle<()>> {
+        let server = RpcServer { map: RwLock::new(HashMap::new()) };
+        server.register(get_dispatch_register());
+        conetty::TcpServer::start(server, addr)
+    }
+
+    pub fn register(&self, map: &'static [(u64, DispatchFn)]) {
+        let mut w_map = self.map.write().unwrap();
+
+        for &(req_id, f) in map {
+            match w_map.insert(req_id, f) {
+                Some(_) => warn!("register rpc updated. req_id={}", req_id),
+                None => info!("register rpc. req_id={}", req_id),
+            }
+        }
+    }
+}
 
 
 // rpm_impl! {
@@ -19,25 +69,59 @@ extern crate log;
 // }
 // }
 
-// rpc spec
-trait EchoRpc: Send + Sync + 'static {
-    fn echo(&self, data: String) -> String;
-    fn add(&self, x: u32, y: u32) -> u32;
-}
-
-// server implementation
-// impl EchoRpc for Echo {
-//     fn echo(data: String) -> String {
-//         data
-//     }
-//
-//     fn add(x: u32, y: u32) -> u32 {
-//         x + y
-//     }
-// }
-
 // ------------------------------------------------------------------------------------------------
 // below should be generated code
+
+// rpc impl
+fn echo(data: String) -> String {
+    data
+}
+
+fn add(x: u32, y: u32) -> u32 {
+    x + y
+}
+
+fn dispatch_req(req_id: u64,
+                req: &[u8],
+                rsp: &mut conetty::RspBuf)
+                -> Result<(), conetty::WireError> {
+    use bincode as encode;
+    use bincode::SizeLimit::Infinite;
+    use std::io::Cursor;
+
+    let mut input = Cursor::new(req);
+
+    // deserialize the request
+    let _id: u64 = encode::deserialize_from(&mut input, Infinite)
+        .map_err(|e| conetty::WireError::ServerDeserialize(e.to_string()))?;
+    debug_assert_eq!(_id, req_id);
+
+    // dispatch call the service
+    if req_id == tocken_id!(echo) {
+        // 0 => {
+        let (data,): (String,) = encode::deserialize_from(&mut input, Infinite)
+                .map_err(|e| conetty::WireError::ServerDeserialize(e.to_string()))?;
+        let ret = echo(data);
+        // serialize the result
+        encode::serialize_into(rsp, &ret, Infinite)
+            .map_err(|e| conetty::WireError::ServerSerialize(e.to_string()))
+    } else if req_id == tocken_id!(add) {
+        let (x, y): (u32, u32) = encode::deserialize_from(&mut input, Infinite)
+                .map_err(|e| conetty::WireError::ServerDeserialize(e.to_string()))?;
+        let ret = add(x, y);
+        // serialize the result
+        encode::serialize_into(rsp, &ret, Infinite)
+            .map_err(|e| conetty::WireError::ServerSerialize(e.to_string()))
+    } else {
+        unreachable!("unkown req_id = {}", req_id);
+    }
+}
+
+pub fn get_dispatch_register() -> &'static [(u64, DispatchFn)] {
+    const HANDLER_MAP: &'static [(u64, DispatchFn)] = &[(tocken_id!(echo), dispatch_req),
+                                                        (tocken_id!(add), dispatch_req)];
+    HANDLER_MAP
+}
 
 pub trait RpcClient: conetty::Client {
     fn echo(&self, arg0: String) -> Result<String, conetty::Error> {
@@ -84,60 +168,6 @@ pub trait RpcClient: conetty::Client {
 impl RpcClient for conetty::MultiplexClient {}
 impl RpcClient for conetty::TcpClient {}
 
-
-use std::collections::HashMap;
-use conetty::coroutine::sync::RwLock;
-pub type DispatchFn = &'static fn(u64, &[u8], &mut conetty::RspBuf) -> Result<(), conetty::WireError>;
-pub struct RpcServer {
-    map: RwLock<HashMap<u64, DispatchFn>>,
-}
-
-impl conetty::Server for RpcServer {
-    fn service(&self, req: &[u8], rsp: &mut conetty::RspBuf) -> Result<(), conetty::WireError> {
-        use bincode as encode;
-        use bincode::SizeLimit::Infinite;
-
-        // deserialize the request
-        let req_id: u64 = encode::deserialize(req)
-            .map_err(|e| conetty::WireError::ServerDeserialize(e.to_string()))?;
-
-        info!("req_id = {}", req_id);
-        // get the dispatch_fn
-        let f = {
-            let r_map = self.map.read().unwrap();
-            match r_map.get(&req_id) {
-                Some(f) => *f,
-                None => return Err(conetty::WireError::Status(404)),
-            }
-        };
-
-        f(req_id, req, rsp)
-        // // dispatch call the service
-        // match req {
-        //     EchoRpcEnum::hello((arg0,)) => {
-        //         let ret = self.echo(arg0);
-        //         // serialize the result
-        //         encode::serialize_into(rsp, &ret, Infinite)
-        //             .map_err(|e| conetty::WireError::ServerSerialize(e.to_string()))
-        //     }
-        //     EchoRpcEnum::add((arg0, arg1)) => {
-        //         let ret = self.add(arg0, arg1);
-        //         // serialize the result
-        //         encode::serialize_into(rsp, &ret, Infinite)
-        //             .map_err(|e| conetty::WireError::ServerSerialize(e.to_string()))
-        //     }
-        // }
-    }
-}
-
-impl RpcServer {
-    pub fn start<L: ::std::net::ToSocketAddrs>
-        (addr: L)
-         -> ::std::io::Result<conetty::coroutine::JoinHandle<()>> {
-        let server = RpcServer { map: RwLock::new(HashMap::new()) };
-        conetty::TcpServer::start(server, addr)
-    }
-}
 // ------------------------------------------------------------------------------------------------
 
 fn main() {
